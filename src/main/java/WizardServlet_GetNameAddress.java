693019d4e9af234262edfc9f99c74beaaadd3abf
Pernyei Gábor <45963704+Narghil@users.noreply.github.com>
2020-04-15 11:01:28 +0200

    Fix charset error

--- src/main/java/WizardServlet_GetNameAddress.java
+++ src/main/java/WizardServlet_GetNameAddress.java
@@ -23,11 +23,11 @@ public class WizardServlet_GetNameAddress extends HttpServlet {
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter out = resp.getWriter();
         out.println("<!DOCTYPE html><html lang=hu><head><meta charset=UTF-8><title>Rendelés összesítése</title></head>");
-        out.println("<body><h1>Rendelesed:</h1>");
+        out.println("<body><h1>Rendelésed:</h1>");
         out.println("Session ID:" + session.getId()+"<br>" );
-        out.println("Rendelt termek:" + session.getAttribute("cbGoods")+"<br>" );
+        out.println("Rendelt termék:" + session.getAttribute("cbGoods")+"<br>" );
         out.println("Neved:" + req.getParameter("tfName")+"<br>");
-        out.println("Cimed:" + req.getParameter("tfAddress")+"<br>");
+        out.println("Címed:" + req.getParameter("tfAddress")+"<br>");
         out.println("<a href=\"index.html\" target=\"_self\">Rendben, fizetek!</a>");
         out.println("</body></html>");
         session.invalidate();
